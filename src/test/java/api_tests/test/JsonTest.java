c1997538653fb6ba7df1134f224a6079951787bb
itlm38chel <itlm38chel@gmail.com>
2023-08-27 20:49:57 +0500

    Все сделал, вроде работает

--- src/test/java/api_tests/test/JsonTest.java
+++ src/test/java/api_tests/test/JsonTest.java
@@ -1,9 +1,11 @@
 package api_tests.test;
 
 import api_tests.actions.JsonPlaceholder;
+import api_tests.pojo.Album;
+import api_tests.pojo.Comment;
+import api_tests.pojo.Photo;
 import api_tests.pojo.Post;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import lombok.Data;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -32,7 +34,7 @@ public class JsonTest {
 
     @Test
     public void comments() throws JsonProcessingException {
-        List<CommentClass> obj = jsonplaceholder.comments(url);
+        List<Comment> obj = jsonplaceholder.comments(url);
         for (int i = 0; i < obj.size(); i++) {
             Assert.assertNotNull(obj.get(i).getPostId());
             Assert.assertNotNull(obj.get(i).getId());
@@ -45,7 +47,7 @@ public class JsonTest {
 
     @Test
     public void albums() throws JsonProcessingException {
-        List<AlbumClass> obj = jsonplaceholder.albums(url);
+        List<Album> obj = jsonplaceholder.albums(url);
         for (int i = 0; i < obj.size(); i++) {
             Assert.assertNotNull(obj.get(i).getUserId());
             Assert.assertNotNull(obj.get(i).getId());
@@ -56,7 +58,7 @@ public class JsonTest {
 
     @Test
     public void photos() throws JsonProcessingException {
-        List<PhotoClass> obj = jsonplaceholder.photos(url);
+        List<Photo> obj = jsonplaceholder.photos(url);
         for (int i = 0; i < obj.size(); i++) {
             Assert.assertNotNull(obj.get(i).getAlbumId());
             Assert.assertNotNull(obj.get(i).getId());
@@ -65,30 +67,5 @@ public class JsonTest {
             Assert.assertNotNull(obj.get(i).getThumbnailUrl());
         }
         Assert.assertEquals(obj.size(), photos);
-    }
-
-    @Data
-    public static class CommentClass {
-        private Integer postId;
-        private Integer id;
-        private String name;
-        private String email;
-        private String body;
-    }
-
-    @Data
-    public static class AlbumClass {
-        private Integer userId;
-        private Integer id;
-        private String title;
-    }
-
-    @Data
-    public static class PhotoClass {
-        private Integer albumId;
-        private Integer id;
-        private String title;
-        private String url;
-        private String thumbnailUrl;
     }
 }
