777d2db7cc1d11c1e3bf01578a31b5879c0b7e95
itlm38chel <itlm38chel@gmail.com>
2023-08-27 10:44:39 +0500

    Перенес @DATA классы в JsonTest

---
+++ src/test/java/api_tests/pojo/Comment.java
@@ -0,0 +1,29 @@
+package api_tests.pojo;
+
+public class Comment {
+    private Integer postId;
+    private Integer id;
+    private String name;
+    private String email;
+    private String body;
+
+    public Integer getPostId() {
+        return postId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getBody() {
+        return body;
+    }
+}
